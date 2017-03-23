a4753ce4a361e32f720c0e2992a274cb2d6c7c05
VISWESWARAN1998 <visweswaran.nagasivam98@gmail.com>
2017-03-23 23:38:36 +0530

    Another few updates

--- Server.java
+++ Server.java
@@ -1,7 +1,29 @@
 // Author : Visweswaran
 import java.net.*;
+import java.io.*;
 
-
-public class Server {
-
+class Server 
+{
+public static void main(String[] args) {
+	ServerSocket server = null;
+	try
+	{
+		// Open the port no-666
+		server = new ServerSocket(666);
+		Socket socket = server.accept();
+		System.out.println("Connection receiverd @ "+socket.getLocalPort()+" from"+socket.getRemoteSocketAddress());
+		// Client initiates the communication between the sockets
+		DataInputStream input = new DataInputStream(socket.getInputStream());
+		System.out.println(input.readUTF());
+		// Now server will send the connection
+		DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+		output.writeUTF("Connected!");
+		socket.close();
+		server.close();
+	}
+	catch(IOException e)
+	{
+		
+	}
+}
 }
